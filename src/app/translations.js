95675ed347f38f12b4fb8d1f05fa4e0935e9b737
katriyna <katriyna@gmail.com>
2018-09-13 20:41:18 +0300

    try to prevent overriding translations by translations from other widgets

--- src/app/translations.js
+++ src/app/translations.js
@@ -1,7 +1,7 @@
-import {de} from './translations/locale_de.po';
-import {es} from './translations/locale_es.po';
-import {fr} from './translations/locale_fr.po';
-import {ru} from './translations/locale_ru.po';
-import {ja} from './translations/locale_ja.po';
+import {de} from './translations/youtrack-board-status-widget_de.po';
+import {es} from './translations/youtrack-board-status-widget_es.po';
+import {fr} from './translations/youtrack-board-status-widget_fr.po';
+import {ru} from './translations/youtrack-board-status-widget_ru.po';
+import {ja} from './translations/youtrack-board-status-widget_ja.po';
 
 export default {de, es, fr, ru, ja};
